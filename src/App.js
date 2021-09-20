d34d2f6a59ce5d0bbd0a1e8204648b1d9ad1fd99
DESKTOP-U3OVTMD\ilove <ilovegumi0118@gmail.com>
2021-09-20 22:31:05 +0900

    exchange file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-function
-  App() {
-  return <h1>Hellow World !!</h1>
+// chips:Reactパッケージのインポートは要らない模様。
+import { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <label htmlFor="bar">default</label>
+        <input type="text" onChange={() => { console.log('hellow!!') }} />
+        {/* // chips:以下の様な書き方も可能
+            // const greeting = "MiMi";
+            // const dom = <h1 className="">{greeting}</h1>;
+            // >> クォーテーションの囲いが要らない
+            // >> 変数を出力する場合は{ }で囲む
+            // >> JSXの文法上、classではなくclassNameを使用する
+            // return dom 
+        */}
+      </>
+    )
+  }
 }
 
 export default App;
