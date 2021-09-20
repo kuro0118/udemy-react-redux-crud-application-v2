7417cdecf271b96480b27294d471734d9df92fde
DESKTOP-U3OVTMD\ilove <ilovegumi0118@gmail.com>
2021-09-20 22:49:40 +0900

    components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-// chips:Reactパッケージのインポートは要らない模様。
-import { Component } from 'react'
+// chips:クラスコンポーネントを使用しない場合は以下のimportは不要
+// import { Component } from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <label htmlFor="bar">default</label>
-        <input type="text" onChange={() => { console.log('hellow!!') }} />
-        {/* // chips:以下の様な書き方も可能
-            // const greeting = "MiMi";
-            // const dom = <h1 className="">{greeting}</h1>;
-            // >> クォーテーションの囲いが要らない
-            // >> 変数を出力する場合は{ }で囲む
-            // >> JSXの文法上、classではなくclassNameを使用する
-            // return dom 
-        */}
-      </>
-    )
-  }
+// chips:以下がクラスコンポーネント 
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//         <label htmlFor="bar">default</label>
+//         <input type="text" onChange={() => { console.log('hellow!!') }} />
+//       </>
+//     )
+//   }
+// }
+
+
+// chips:以下がファンクションコンポーネント
+const App = () => {
+  return (
+    <>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </>
+  )
+}
+
+const Cat = () => {
+  return <div>Odamari!!</div>
 }
 
 export default App;
